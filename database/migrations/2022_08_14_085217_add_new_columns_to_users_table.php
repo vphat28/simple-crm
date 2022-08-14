b9eccaafbc696fb61384407ba93dadd1657762e9
Xavi Nguyen <vphat28@gmail.com>
2022-08-14 20:02:06 +0700

    Add extra data to user table

---
+++ database/migrations/2022_08_14_085217_add_new_columns_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone_number', 20);
+            $table->string('email_address', 100);
+            $table->float('desired_budget');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['phone_number', 'email_address', 'desired_budget']);
+        });
+    }
+}
