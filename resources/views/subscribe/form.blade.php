bbd12e29a76cbad7429bc8316e3daa1e31e87a5e
Xavi Nguyen <vphat28@gmail.com>
2022-08-14 15:50:13 +0700

    Email address input

--- resources/views/subscribe/form.blade.php
+++ resources/views/subscribe/form.blade.php
@@ -26,6 +26,12 @@
                 </div>
             </div>
             <div class="flex flex-wrap -mx-3 mb-2">
+                <div class="w-full px-3 mb-6 md:mb-0">
+                    <label for="budget" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Email Address</label>
+                    <input type="text" type="email" id="email-address" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" required>
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-2">
                 <div class="w-full md:mb-0 px-3">
                     <label for="message" class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Message</label>
                     <textarea class="w-full rounded" id="message">
